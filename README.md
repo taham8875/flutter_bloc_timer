7b5ec30c82d8b8b5bd8f67b1c9fab558e55b473d
taham8875 <taha.m8875@gmail.com>
2024-05-17 14:09:03 +0300

    feat: Add initial project files and configurations

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_bloc_timer
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_bloc_timer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_bloc_timer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
